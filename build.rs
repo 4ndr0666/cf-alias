4b49022b7169f577a05aea6bf3ce1cbc2a3d1c83
4ndr0666 <01_dolor.loftier@icloud.com>
2025-10-29 18:55:26 -0500

    updated build.rs the enum variant to inherit not inherited

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
-use cargo_toml::Manifest;
+use cargo_toml::{Inheritable, Manifest};
 use std::path::Path;
 
 fn main() {
     let manifest = Manifest::from_path(Path::new("Cargo.toml")).expect("failed to read Cargo.toml");
     let package = manifest.package.expect("missing [package]");
+
     let version = match package.version {
-        cargo_toml::Inheritable::Inherit(_) => "unknown".to_string(),
-        cargo_toml::Inheritable::Set(v) => v,
+        Inheritable::Inherited(_) => "unknown".to_string(),
+        Inheritable::Set(v) => v,
     };
+
     println!("cargo:rustc-env=CFA_VERSION=v{}", version);
 }
