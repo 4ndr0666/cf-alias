67359491797b03b642656d7979d6810bdee564b1
4ndr0666 <01_dolor.loftier@icloud.com>
2025-10-29 22:10:12 -0500

    aligned src/main.rs with pinned dependencies and added completions generation at runtime for goreleaser.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ mod cloudflare;
 mod config;
 mod utils;
 
-use std::io;
-use std::fs;
-use std::path::PathBuf;
-
 use anyhow::Result;
-use clap::{Arg, Command};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use clap_complete::{generate_to, Shell};
+use std::{fs, path::PathBuf};
 
 async fn list_routes() -> Result<String> {
     let routes = cloudflare::list_routes().await?;
@@ -30,159 +27,136 @@ async fn create(email_prefix: String) -> Result<String> {
     return Ok(email);
 }
 
-fn build_cli() -> Command {
-    return Command::new("cf-alias")
+fn build_cli<'help>() -> App<'help> {
+    App::new("cf-alias")
         .about("CLI interface for Cloudflare Email Routing")
         .version(env!("CFA_VERSION"))
-        .subcommand_required(true)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(
-            Command::new("alfred")
+            SubCommand::with_name("alfred")
                 .about("Commands for the Alfred extension")
                 .subcommand(
-                    Command::new("clipboard")
+                    SubCommand::with_name("clipboard")
                         .about("Copys email to clipboard.")
                         .arg(
-                            Arg::new("email")
+                            Arg::with_name("email")
                                 .short('e')
                                 .long("email")
                                 .help("Email to copy")
-                                .required(true)
-                                .num_args(1),
+                                .takes_value(true)
+                                .required(true),
                         ),
                 )
                 .subcommand(
-                    Command::new("create")
+                    SubCommand::with_name("create")
                         .about("Creates a new forwarding email")
                         .arg(
-                            Arg::new("email-prefix")
+                            Arg::with_name("email-prefix")
                                 .short('e')
                                 .long("email-prefix")
                                 .help("Forwarding email prefix to create")
-                                .required(true)
-                                .num_args(1),
+                                .takes_value(true)
+                                .required(true),
                         ),
                 )
                 .subcommand(
-                    Command::new("create-list")
+                    SubCommand::with_name("create-list")
                         .about("Autocomplete command used for creating new emails")
                         .arg(
-                            Arg::new("query")
+                            Arg::with_name("query")
                                 .short('q')
                                 .long("query")
                                 .help("Command query or email prefix to be used when creating a new email")
-                                .required(false)
-                                .num_args(1),
+                                .takes_value(true)
+                                .required(false),
                         ),
                 )
-                .subcommand(
-                    Command::new("manage")
-                        .about("Opens the Cloudflare Email management UI.")
-                )
-                .subcommand(
-                    Command::new("list")
-                        .about("List existing email forwarders")
-                ),
+                .subcommand(SubCommand::with_name("manage").about("Opens the Cloudflare Email management UI."))
+                .subcommand(SubCommand::with_name("list").about("List existing email forwarders")),
         )
         .subcommand(
-            Command::new("create")
+            SubCommand::with_name("create")
                 .about("Creates a new forwarding email")
-                .arg_required_else_help(true)
                 .arg(
-                    Arg::new("email-prefix")
+                    Arg::with_name("email-prefix")
                         .short('e')
                         .long("email-prefix")
                         .help("Forwarding email prefix to create")
-                        .required(false)
-                        .num_args(1),
+                        .takes_value(true)
+                        .required(false),
                 )
                 .arg(
-                    Arg::new("random")
+                    Arg::with_name("random")
                         .short('r')
                         .long("random")
                         .help("Generate a random email address")
-                        .required(false)
-                        .num_args(0),
+                        .takes_value(false)
+                        .required(false),
                 ),
         )
         .subcommand(
-            Command::new("completion")
+            SubCommand::with_name("completion")
                 .about("Generates and installs shell completions automatically")
                 .arg(
-                    Arg::new("shell")
+                    Arg::with_name("shell")
                         .short('s')
                         .long("shell")
-                        .help("Which shell to generate completions for.")
-                        .value_parser(clap::builder::EnumValueParser::<Shell>::new())
-                        .required(false)
-                        .num_args(1),
+                        .help("Which shell to generate completions for (bash|zsh|fish)")
+                        .takes_value(true)
+                        .required(false),
                 ),
         )
-        .subcommand(
-            Command::new("list")
-                .about("List existing email routes.")
-        );
+        .subcommand(SubCommand::with_name("list").about("List existing email routes."))
 }
 
 async fn parse_cli() -> Result<()> {
     let matches = build_cli().get_matches();
+
     match matches.subcommand() {
-        Some(("alfred", args)) => {
-            match args.subcommand() {
-                Some(("create", run_matches)) => {
-                    let email_prefix = run_matches
-                        .get_one::<String>("email-prefix")
-                        .unwrap()
-                        .to_string();
-                    alfred::create(email_prefix).await?;
-                }
-                Some(("create-list", run_matches)) => {
-                    let default_res = "".to_string();
-                    let query = run_matches
-                        .get_one::<String>("query")
-                        .unwrap_or_else(|| return &default_res)
-                        .to_string();
-                    let res = alfred::create_list(query)?;
-                    println!("{}", res);
-                }
-                Some(("clipboard", run_matches)) => {
-                    let email = run_matches
-                        .get_one::<String>("email")
-                        .unwrap()
-                        .to_string();
-                    alfred::copy_to_clopboard(email);
-                }
-                Some(("manage", _)) => {
-                    alfred::open_manage()?;
-                }
-                Some(("list", _)) => {
-                    let emails = alfred::list_routes().await?;
-                    println!("{}", emails);
-                }
-                _ => unreachable!(),
+        Some(("alfred", args)) => match args.subcommand() {
+            Some(("create", run_matches)) => {
+                let email_prefix = run_matches.value_of("email-prefix").unwrap().to_string();
+                alfred::create(email_prefix).await?;
             }
-        }
+            Some(("create-list", run_matches)) => {
+                let query = run_matches.value_of("query").unwrap_or("").to_string();
+                let res = alfred::create_list(query)?;
+                println!("{}", res);
+            }
+            Some(("clipboard", run_matches)) => {
+                let email = run_matches.value_of("email").unwrap().to_string();
+                alfred::copy_to_clopboard(email);
+            }
+            Some(("manage", _)) => {
+                alfred::open_manage()?;
+            }
+            Some(("list", _)) => {
+                let emails = alfred::list_routes().await?;
+                println!("{}", emails);
+            }
+            _ => {}
+        },
         Some(("create", run_matches)) => {
-            let default_res = "".to_string();
-            let mut email_prefix = run_matches
-                .get_one::<String>("email-prefix")
-                .unwrap_or_else(|| return &default_res)
-                .to_string();
-            if run_matches.contains_id("random") {
+            let mut email_prefix = run_matches.value_of("email-prefix").unwrap_or("").to_string();
+            if run_matches.is_present("random") {
                 email_prefix = "random".to_string();
             }
             let email = create(email_prefix).await?;
             println!("Created new email: {}", email);
         }
         Some(("completion", run_matches)) => {
-            use clap_complete::{generate, Shell};
+            let shell_opt = run_matches.value_of("shell");
             let mut app = build_cli();
             let outdir = PathBuf::from("completions");
             fs::create_dir_all(&outdir).ok();
-            let shells = if let Some(shell) = run_matches.get_one::<Shell>("shell").copied() {
-                vec![shell]
-            } else {
-                vec![Shell::Bash, Shell::Zsh, Shell::Fish]
+
+            let shells: Vec<Shell> = match shell_opt {
+                Some("bash") => vec![Shell::Bash],
+                Some("zsh") => vec![Shell::Zsh],
+                Some("fish") => vec![Shell::Fish],
+                _ => vec![Shell::Bash, Shell::Zsh, Shell::Fish],
             };
+
             for shell in shells {
                 let target = generate_to(shell, &mut app, "cf-alias", &outdir)
                     .expect("failed to write completion");
@@ -205,8 +179,9 @@ async fn parse_cli() -> Result<()> {
             let emails = list_routes().await?;
             println!("{}", emails);
         }
-        _ => unreachable!(),
+        _ => {}
     }
+
     return Ok(());
 }
 
